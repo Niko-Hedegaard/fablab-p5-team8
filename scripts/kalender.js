3d0ee8e83dd876bc2573fc21dfe28fee96122b2e
Nikolai Hedegaard Hansen <168433171+Niko-Hedegaard@users.noreply.github.com>
2024-12-08 21:06:11 +0100

    Færdig med javascript

--- scripts/kalender.js
+++ scripts/kalender.js
@@ -12,3 +12,44 @@ weekender.forEach((dag) => {
   dagElement.appendChild(dagText);
   kalenderDageContainer.appendChild(dagElement);
 });
+
+let modalLedig = document.querySelector(".oversigt__ledig");
+let modalBooket = document.querySelector(".oversigt__booket");
+let modalBooketMorgen = document.querySelector(".oversigt__booket--morgen");
+let modalBooketAften = document.querySelector(".oversigt__booket--morgen");
+
+function modalLuk() {
+  console.log("luk modalboks");
+  if ((modalLedig.classname = "active")) {
+    modalLedig.classList.remove("active");
+  }
+  if ((modalBooket.classname = "active")) {
+    modalBooket.classList.remove("active");
+  }
+  if ((modalBooketMorgen.classname = "active")) {
+    modalBooketMorgen.classList.remove("active");
+  }
+  if ((modalBooketAften.classname = "active")) {
+    modalBooketAften.classList.remove("active");
+  }
+}
+
+function modalLedigKnap() {
+  console.log("Får tilføjet active");
+  modalLedig.classList.toggle("active");
+}
+
+function modalBooketKnap() {
+  console.log("Får tilføjet active");
+  modalBooket.classList.toggle("active");
+}
+
+function modalBooketMorgenKnap() {
+  console.log("Får tilføjet active");
+  modalBooketMorgen.classList.toggle("active");
+}
+
+function modalBooketAftenKnap() {
+  console.log("Får tilføjet active");
+  modalBooketAften.classList.toggle("active");
+}
